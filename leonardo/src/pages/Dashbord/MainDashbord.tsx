4b4670dddaf01d9d642bc95113528282711060c2
Andrea <0andreaf0@gmail.com>
2024-06-20 16:20:49 +0200

    daje roma daje 2

--- leonardo/src/pages/Dashbord/MainDashbord.tsx
+++ leonardo/src/pages/Dashbord/MainDashbord.tsx
@@ -1,13 +1,7 @@
-import React from 'react'
-import { EmblaOptionsType } from 'embla-carousel'
-import LNavBar from "../LeonardoNavBar";
-import EmblaCarousel from './carousel/EmblaCarousel';
+  import LNavBar from "../LeonardoNavBar";
 
 
 export const Dashboard = () => {
-  const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true}
-  const SLIDE_COUNT = 5
-  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 
 
@@ -16,7 +10,6 @@ export const Dashboard = () => {
       <div className="absolute inset-0 w-full h-full bg-loginForm bg-contain bg-no-repeat bg-center filter opacity-20"></div>
       <LNavBar icon={1}></LNavBar>
       <div className="flex flex-col md:flex-row justify-center items-center w-[100%] h-full rounded-lg">
-        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
       </div>
     </div>
   );
