3cd41729ecd23ae87e55e318f9d7c56a304a07c5
Osema <ofadhel@ofadhel.ofadhel>
2024-06-22 19:18:19 +0200

    added only the card component and changed index.tsx to show page

--- leonardo/src/pages/HomePage/HomePage.tsx
+++ leonardo/src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import EmblaCarousel from '../Dashbord/carousel/EmblaCarousel'
-import { EmblaOptionsType } from 'embla-carousel'
+import React, { useState } from 'react';
+import { ThreeDCardDemo } from './card/cardhome';
 
-export const HomePage : React.FC = () => {
-  const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true}
-  const SLIDE_COUNT = 5
-  const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+export const HomePage: React.FC = () => {
+  //const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true };
+  //const SLIDE_COUNT = 5;
+  //const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+  //const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
     <div className="relative flex justify-center items-center w-full h-[100vh] bg-leonardoRed">
-      <div className="absolute inset-0 w-full h-full bg-loginForm bg-contain bg-no-repeat bg-center filter opacity-20 "></div>
-      <div className="relative z-10 w-full h-full">
-        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      <div className="flex overflow-x-auto">
+        <div className="flex justify-center">
+          <div className="flex space-x-4">
+            <ThreeDCardDemo />
+            <ThreeDCardDemo />
+            <ThreeDCardDemo />
+            <ThreeDCardDemo />
+          </div>
+        </div>
       </div>
     </div>
-
   );
 };
--- leonardo/src/routes/index.tsx
+++ leonardo/src/routes/index.tsx
@@ -1,15 +1,8 @@
 
 import Dashbord from '@/pages/Dashbord/MainDashbord'
-import { useSession } from '../context/sessionProvider'
 import { createFileRoute } from '@tanstack/react-router'
+import { HomePage } from '@/pages/HomePage/HomePage'
 
 export const Route = createFileRoute('/')({
-  component: () => {
-
-    const session = useSession()
-
-    console.log("daje roma daje ",session?.user.name)
-
-    return <Dashbord></Dashbord>
-  }
+  component: () => <HomePage/>
 })
